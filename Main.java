ab4679c7f44b6bb54d627177b43012e901d5df3b
burcj3622 <burcj3622@wrdsb.ca>
2020-10-06 13:00:40 +0000

    multiplication

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -10,6 +11,19 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+   {
+   int j,n;
+
+   System.out.print("Please enter an integer to create a multiplication table for: ");
+{
+    Scanner in = new Scanner(System.in);
+		    n = in.nextInt();
+
+   System.out.println ("\n");
+   for(j=0;j<=n;j++)
+  
+     System.out.println(n+" X "+j+" = " +n*j);
+   }
+   }
   }
 }
